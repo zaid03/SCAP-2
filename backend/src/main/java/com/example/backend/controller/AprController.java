b28939c701d0736ed18891f50fe58bee5b770500
zaid03 <terguyzaid@gmail.com>
2026-06-09 14:27:13 +0100

    add provs to art api

--- backend/src/main/java/com/example/backend/controller/AprController.java
+++ backend/src/main/java/com/example/backend/controller/AprController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -100,6 +101,42 @@ public class AprController {
 
             aprRepository.deleteById(id);
             return ResponseEntity.noContent().build();
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ERROR + ex.getMessage());
+        }
+    }
+
+    //adding proveedores to an articulo
+    public record provsAdd(Integer ENT, String AFACOD, String ASUCOD, String ARTCOD, List<Integer> tercods) {};
+    @PostMapping("/add-terceros")
+    public ResponseEntity<?> tercerodAdd (
+        @RequestBody provsAdd payload
+    ) {
+        try {
+            if (payload == null || payload.ENT() == null || payload.AFACOD() == null || payload.ASUCOD() == null || payload.ARTCOD() == null || payload.tercods() == null) {
+                return ResponseEntity.badRequest().body("Faltan datos obligatorios.");
+            }
+
+            Integer count = 0;
+            for(Integer tercod : payload.tercods()) {
+                AprId id = new AprId(payload.ENT(), tercod, payload.AFACOD() ,payload.ASUCOD(), payload.ARTCOD());
+                if (!aprRepository.existsById(id)) {
+                    Apr articuloAdd = new Apr();
+                    articuloAdd.setENT(payload.ENT());
+                    articuloAdd.setAFACOD(payload.AFACOD());
+                    articuloAdd.setASUCOD(payload.ASUCOD());
+                    articuloAdd.setARTCOD(payload.ARTCOD());
+                    articuloAdd.setTERCOD(tercod);
+                    articuloAdd.setAPRACU(0);
+                    articuloAdd.setAPRPRE(0.00);
+                    articuloAdd.setAPRUEM(0.00);
+                    aprRepository.save(articuloAdd);
+                    count = count + 1;
+                }
+
+            }
+
+            return ResponseEntity.ok("Se guardaron " + count + " artículos");
         } catch (Exception ex) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ERROR + ex.getMessage());
         }
