c186c06f0044c45eba11bb7f286d7939e9e25fd7
zaid03 <terguyzaid@gmail.com>
2026-09-15 17:22:02 +0100

    added search service with related criteria

--- backend/src/main/java/com/example/backend/controller/CogController.java
+++ backend/src/main/java/com/example/backend/controller/CogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.example.backend.dto.SaldoContrato;
 import com.example.backend.sqlserver2.repository.CogRepository;
+import com.example.backend.service.SaldoContratoSearch;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -16,6 +17,8 @@ import org.springframework.http.HttpStatus;
 public class CogController {
     @Autowired
     private CogRepository cogRepository;
+    @Autowired
+    private SaldoContratoSearch saldoContratoSearch;
 
     private static final String SIN_RESULTADO = "Sin resultado";
     private static final String ERROR = "Error :";
@@ -27,14 +30,39 @@ public class CogController {
         @PathVariable String eje
     ) {
         try {
-            List<SaldoContrato> contatos = cogRepository.findByENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONot(ent, eje, 3, 1);
-            if (contatos.isEmpty()) {
+            List<SaldoContrato> contratos = cogRepository.findByENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONot(ent, eje, 3, 1);
+            if (contratos.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(SIN_RESULTADO);
             }
 
-            return ResponseEntity.ok(contatos);
+            return ResponseEntity.ok(contratos);
         } catch (DataAccessException ex) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ERROR + ex.getMostSpecificCause().getMessage());
         }
+    }
+
+    //filtering for C.saldo de contrato
+    @GetMapping("/search-saldo-contrato/{ent}/{eje}")
+    public ResponseEntity<?> searchSaldoContrato (
+        @PathVariable Integer ent,
+        @PathVariable String eje,
+        @RequestParam(required = false) String cge,
+        @RequestParam(required = false) String contrato,
+        @RequestParam(required = false) String proveedor
+    ) {
+        try {
+            if (cge == null && contrato == null && proveedor == null) {
+            return ResponseEntity.badRequest().body("Faltan datos obligatorios");
+            }
+
+            List<SaldoContrato> contratos = saldoContratoSearch.searchSaldoContratos(ent, eje, cge, contrato, proveedor);
+            if (contratos.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(SIN_RESULTADO);
+            }
+
+            return ResponseEntity.ok(contratos);
+        } catch (DataAccessException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ERROR + ex.getMostSpecificCause().getMessage());
+        }  
     }
 }
--- backend/src/main/java/com/example/backend/sqlserver2/repository/CogRepository.java
+++ backend/src/main/java/com/example/backend/sqlserver2/repository/CogRepository.java
@@ -10,4 +10,13 @@ import java.util.List;
 public interface CogRepository extends JpaRepository<Cog, CogId> {
   //main fetch for C.saldo de contrato
   List<SaldoContrato> findByENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONot(Integer ent, String eje, Integer contip, Integer conblo);
+
+  //filtering for C.saldo de contrato
+  List<SaldoContrato> findByENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONotAndCge_CGECOD(Integer ent, String eje, Integer contip, Integer conblo, String cgecod);
+  List<SaldoContrato> findByENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONotAndCONCOD(Integer ent, String eje, Integer contip, Integer conblo, Integer concod);
+  List<SaldoContrato> findByENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONotAndCot_conn_CONDESContaining(Integer ent, String eje, Integer contip, Integer conblo, String condes);
+  List<SaldoContrato> findByENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONotAndCot_ter_TERCODOrENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONotAndCot_ter_TERNIFContaining(Integer ent1, String eje1, Integer contip1, Integer conblo1, Integer tercod, Integer ent2, String eje2, Integer contip2, Integer conblo2, String ternif);
+  List<SaldoContrato> findByENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONotAndCot_ter_TERNOMContainingOrENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONotAndCot_ter_TERNIFContaining(Integer ent1, String eje1, Integer contip1, Integer conblo1, String ternom, Integer ent2, String eje2, Integer contip2, Integer conblo2, String ternif);
+  List<SaldoContrato> findByENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONotAndCge_CGECODAndCONCOD(Integer ent, String eje, Integer contip, Integer conblo, String cgecod, Integer concod);
+  List<SaldoContrato> findByENTAndEJEAndCot_conn_CONTIPAndCot_conn_CONBLONotAndCge_CGECODAndCot_conn_CONDESContaining(Integer ent, String eje, Integer contip, Integer conblo, String cgecod, String condes);
 }
